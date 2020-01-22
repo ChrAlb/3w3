332885a407b334760c5ceb34ee5ecc8a7b1af127
Christian Albrecht <albchr@outlook.com>
2020-01-22 20:29:30 +0100

    workin

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,6 @@
 #include "Game.h"
 Game::Game(): m_window("3w3", sf::Vector2u(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height)),
-	m_stateManager(&m_context), m_entityManager(&m_context, 100)
+	m_stateManager(&m_context), m_entityManager(&m_context, 100), m_guiManager(m_window.GetEventManager(),&m_context)
 {
 	m_clock.restart();
 	srand(time(nullptr));
@@ -8,20 +8,31 @@ Game::Game(): m_window("3w3", sf::Vector2u(sf::VideoMode::getDesktopMode().width
 	m_context.m_wind = &m_window;
 	m_context.m_eventManager = m_window.GetEventManager();
 	m_context.m_textureManager = &m_textureManager;
+	m_context.m_guiManager = &m_guiManager;
 	m_context.m_entityManager = &m_entityManager;
-
+	m_fontManager.RequireResource("Main");
 	m_stateManager.SwitchTo(StateType::Intro);
 }
 
-Game::~Game(){}
+Game::~Game()
+{
+	m_fontManager.ReleaseResource("Main");
+}
 
 sf::Time Game::GetElapsed(){ return m_clock.getElapsedTime(); }
 void Game::RestartClock(){ m_elapsed = m_clock.restart(); }
 Window* Game::GetWindow(){ return &m_window; }
 
+
 void Game::Update(){
 	m_window.Update();
 	m_stateManager.Update(m_elapsed);
+	m_context.m_guiManager->Update(m_elapsed.asSeconds());
+	GUI_Event guiEvent;
+	while (m_context.m_guiManager->PollEvent(guiEvent))
+	{
+		m_window.GetEventManager()->HandleEvent(guiEvent);
+	}
 }
 
 void Game::Render(){
